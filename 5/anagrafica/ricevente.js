f71273e3625f574aa26ebce52420723b50d845a0
Alessiomazza07 <149371983+Alessiomazza07@users.noreply.github.com>
2024-11-29 18:10:41 +0100

    Update ricevente.js

--- 5/anagrafica/ricevente.js
+++ 5/anagrafica/ricevente.js
@@ -14,29 +14,3 @@ for(let r=0;r<m.length;r++){
     tableRow.append(col);
     table.append(tableRow); //aggiunta riga alla tabella
 }
-
-
-
-/*const table=document.getElementById("table"); //identificazione tabella creata
-let data = localStorage.getItem("data");
-let m = data.split(",");
-let len = localStorage.getItem("length");
-let mat=[], i=0;
-for(let r=0;r<m.length/len;r++){
-    mat[r]=[];
-    for(let c=0;c<len;c++){
-        mat[r][c]=m[i];
-        i++;
-    }
-}
-for(let j=0;j<mat.length;j++){
-    let tableRow= document.createElement('tr'); //creazione riga
-    let s="tableRow".concat(j);
-    tableRow.className=s;
-    for(let i=0;i<mat[0].length;i++){
-        let col=document.createElement('td');
-        col.innerText = mat[j][i];
-        tableRow.append(col);
-    }
-    table.append(tableRow); //aggiunta riga alla tabella
-}*/
